d6667f75cfd1992fc32bf7c663ab308f8be874eb
Abigail Watson <awatson1978@gmail.com>
2013-08-24 22:20:53 -0400

    header navbar created; router installed; default pages created

---
+++ client/routes.js
@@ -0,0 +1,13 @@
+Router.map(function() {
+    this.route('landingPage', {path: '/'});
+    this.route('rulesPage', {path: '/rules'});
+    this.route('welcomePage', {path: '/welcome'});
+    this.route('mapPage', {path: '/map'});
+    this.route('welcomePage', {path: '/forum'});
+    this.route('welcomePage', {path: '/articles'});
+});
+
+
+
+
+
