133b90016a9708397fd485920294feca933bc3e9
Tarang Patel <tarangp@me.com>
2013-12-08 22:59:52 +0200

    Avatars
    Not sure about this commit. But if there is intent to have 3rd party
    logins this snaps the avatars up

--- server/server.helpers.js
+++ server/server.helpers.js
@@ -3,3 +3,12 @@ Crypto_MD5 = function(text) {
 	var hash = crypto.createHash('md5').update(text).digest('hex');
 	return hash;
 }
+
+getUserService = function(user) {
+	for(var service in user.services) {
+		if(service === "facebook") return "facebook";
+		if(service === "github") return "github";
+		if(service === "twitter") return "twitter";
+		if(service === "google") return "google";
+	}
+}
