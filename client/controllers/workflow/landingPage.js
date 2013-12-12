cdf93e2f27271047f37738e60da27d5a3481ff18
Abigail Watson <awatson1978@gmail.com>
2013-12-11 23:15:07 -0500

    rotating screenshots

--- client/controllers/workflow/landingPage.js
+++ client/controllers/workflow/landingPage.js
@@ -10,10 +10,33 @@ Template.landingPage.events({
     Session.set('show_reactive_overlay', true);
     Session.set('show_overlay_image', true);
 
-//    if(Session.get('show_reactive_overlay')){
-//      Session.set('show_reactive_overlay', false);
-//    }else{
-//      Session.set('show_reactive_overlay', true);
-//    }
   }
 });
+
+Session.setDefault('screenshotIndex', 0);
+Meteor.startup(function () {
+  var displayedScreenshotIndex = 0;
+  Meteor.setInterval(function () {
+    Session.set('screenshotIndex', displayedScreenshotIndex);
+    if(displayedScreenshotIndex == 2){
+      displayedScreenshotIndex = 0;
+    }else{
+      displayedScreenshotIndex++;
+    }
+  }, 3000);
+});
+
+Template.landingPage.getScreenshotPath = function(){
+  switch(Session.get('screenshotIndex')){
+    case 0:
+      return "/images/landingPage/red-theme-example.png";
+      break;
+    case 1:
+      return "/images/landingPage/green-theme-example.png";
+      break;
+    case 2:
+      return "/images/landingPage/blue-theme-example.png";
+      break;
+  }
+
+};
